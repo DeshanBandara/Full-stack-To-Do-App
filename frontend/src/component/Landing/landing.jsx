2ae51e21f7245a3c4652a506296c55d32c929e43
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-15 14:44:51 +0530

    import css file

--- frontend/src/component/Landing/landing.jsx
+++ frontend/src/component/Landing/landing.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './landing.css'
-import {Link} from 'react-scroll'
+import {Link} from 'react-router-dom'
 import Bg from '../../assets/image1.jpeg'
 
 const Landing = () => {
@@ -13,8 +13,8 @@ const Landing = () => {
                 <span className='landingText'>This is <span className='landingName'>To Do App</span></span>
                 <p className='landingPara'>Now you can manage your works as you go.</p>
                 <div className='btnContainer'>
-                    <Link><button className='registerBtn'>Register</button></Link>
-                    <Link><button className='loginBtn'>Login</button></Link>
+                    <button className='registerBtn'>Register</button>
+                    <button className='loginBtn'>Login</button>
                 </div>
             </div>
             <img src={Bg} alt='homeImage' className='bg' />
