a71c7f161bd61a40b42bf7f4a4ba9b620d811ac0
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-15 23:25:18 +0530

    added login page

--- frontend/src/component/Login/login.jsx
+++ frontend/src/component/Login/login.jsx
@@ -1,10 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 
 const Login = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <div>login</div>
+    <section id='login'>
+      <div className='containerLogin'>
+        <div>
+          <h2 className='loginH2'>Login</h2>
+          <hr className='loginHr' />
+          <form className='loginForm'>
+            <div className='loginDiv'>
+              <label className='emailLabel' htmlFor='email'>Email</label>
+              <input
+                type='text'
+                placeholder='Enter Email'
+                autoComplete='off'
+                name='email'
+                className='emailInput'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className='passwordLabel' htmlFor='name'>Password</label>
+              <input
+                type='password'
+                placeholder='Enter Password'
+                autoComplete='off'
+                name='password'
+                className='passwordInput'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className='submitBtn' type='submit'>Login</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
