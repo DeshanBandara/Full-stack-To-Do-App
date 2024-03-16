8d154fabb00691636efa86d9afb418843aa51d53
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-16 17:28:31 +0530

    import hom.jsx and dashboard.jsx files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './component/Landing/landing' //Landing component
 import Register from './component/Register/register' //Register component
 import Login from './component/Login/login'
+import Dashboard from './component/Dashboard/dashboard'
+import Home from './component/Home/home'
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
         <Route exact path='/' Component={Landing} />
         <Route path='/Register' Component={Register} />
         <Route path='/Login' Component={Login} />
+        <Route path='/Dashboard' Component={Dashboard} />
+        <Route path='/Home' Component={Home} />
       </Routes>
     </BrowserRouter>
   )
