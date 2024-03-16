6d612fb5b89c8f8bb3a15f30ae497b5a3e391a77
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-16 21:58:22 +0530

    added dashbord and added navigation parts

--- frontend/src/component/Dashboard/dashboard.jsx
+++ frontend/src/component/Dashboard/dashboard.jsx
@@ -1,10 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './dashboard.css'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const [suc, setSuc]  = useState()
+  axios.defaults.withCredentials = true
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/dashboard`)
+    .then(res => {
+      if (res.data === "Success") {
+        setSuc("Succeeded Ok")
+      } else {
+        navigate('/Login')
+        //navigate('/Landing')
+      }
+    })
+    .catch(err => console.log(err))
+  }, [navigate])
+
   return (
-    <div>dashboard</div>
+    <div>
+      <h2>Dashboard</h2>
+      <p>{suc}</p>
+    </div>
   )
 }
 
