10ecbb39c7a38ba6e76eb02737571124a3dca2b0
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-16 09:44:42 +0530

    Added index .js file and  sets up a basic Express.js server with some middleware for handling requests, connecting to a MongoDB database using Mongoose, enabling CORS (Cross-Origin Resource Sharing), and handling JSON web tokens (JWTs) for authentication

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.listen(3001, () =>{
+    console.log("Server is Running");
+})
