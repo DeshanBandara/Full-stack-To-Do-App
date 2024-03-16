10e5ece6f2b0eeeb7a756ace8e08e1a85f945fb5
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-16 13:09:10 +0530

    added database connections

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,26 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const UserModel = require('./models/user.js') //import user.js file
+const { hash } = require("bcrypt")
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+
+mongoose.connect('mongodb://127.0.0.1:27017/employee');
+
+app.post('/Register', (req, res) => {
+    const {name, email, password} =req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        UserModel.create({name, email, password: hash})
+        .then(user => res.json({status: "ok"}))
+        .catch(err => res.json(err))
+    })
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, () =>{
     console.log("Server is Running");
