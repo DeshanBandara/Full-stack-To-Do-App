3cf09255cd763b019742d3dc691d35e29ac6f754
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-16 17:25:40 +0530

    Added dashboard.jsx and css files

---
+++ frontend/src/component/Dashboard/dashboard.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default Dashboard
