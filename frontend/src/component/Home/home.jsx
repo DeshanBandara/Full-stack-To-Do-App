5953f4c64ccdc33bc23c8fdf8c434e827fcd17d9
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-19 01:13:23 +0530

    Added todo list part

--- frontend/src/component/Home/home.jsx
+++ frontend/src/component/Home/home.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import './home.css'
+import '../../App.css'
 import CreateTask from '../CreateTask/createTask'
 import axios from 'axios'
 import {BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill} from 'react-icons/bs'
+import { Link, useNavigate } from 'react-router-dom'
+
 //import '../../App.css'
 
 const Home = () => {
@@ -35,28 +38,55 @@ const Home = () => {
 
   return (
     <div className='home'>
-      <h2>To Do List</h2>
-      <CreateTask />
-      { todos.length === 0 
-        ?
-        <div><h2>No Records</h2></div>
-        : //else display todos
-          todos.map(todo => (
-            <div className='todoItem' key={todo._id}>
-              <div className='checkBox' onClick={() => handleSelect(todo._id)}>
-                {todo.check ?
-                  <BsFillCheckCircleFill className='icon'></BsFillCheckCircleFill>
-                :
-                  <BsCircleFill className='icon' />
-                }
-                <p className={todo.check ? "line_trough" : ""}>{todo.task}</p>
-              </div>
-              <div>
-                <span><BsFillTrashFill className='icon' onClick={() => handleDelete(todo._id)} /></span>
-              </div>
-            </div>
-          ))
-      }
+        <div className='containerUsers'>
+          <div>
+            <h2>To Do List</h2>
+            <Link to={'/'}><button>Sign Out</button></Link>
+          </div>
+          <Link to='/Create'><button className='addBtn'>Add +</button></Link>
+          <table className='UserTable'>
+              <thead>
+                  <tr>
+                      <th>check</th>
+                      <th>Task</th>
+                      <th>Due Date</th>
+                      <th>Action</th>
+                  </tr>
+              </thead>
+              <tbody>
+                    {todos.length === 0
+                    ?
+                      <tr>
+                        <td><BsCircleFill className='icon' /></td>
+                        <td>No Record</td>
+                        <td>No Record</td>
+                        <td>No Record</td>
+                      </tr>
+                    :
+                      todos.map((todo) => (
+                        <tr key={todo._id}>
+                          <td>
+                            <div className='checkBox' onClick={() => handleSelect(todo._id)}>
+                              {todo.check ?
+                                <BsFillCheckCircleFill className='icon'></BsFillCheckCircleFill>
+                              :
+                                <BsCircleFill className='icon' />
+                              }
+                            </div>
+                          </td>
+                          <td className={todo.check ? "line_trough" : ""}>{todo.task}</td>
+                          <td className={todo.check ? "line_trough" : ""}>{todo.dueDate}</td>
+                          <td>
+                              <Link to={`/taskDetails/${todo._id}`}><button className='editBtn'>Details</button></Link>
+                              <button className='deleteBtn' onClick={() => handleDelete(todo._id)}>Delete</button>
+                          </td>
+                        </tr>
+                      ))
+                    }
+              </tbody>
+          </table>
+      </div>
+
     </div>
   )
 }
