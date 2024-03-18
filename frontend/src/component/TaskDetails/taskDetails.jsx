79dd4f20e5c2281b6b371b9747a7a36e6c1c0e25
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-19 01:13:49 +0530

    Added task details

--- frontend/src/component/TaskDetails/taskDetails.jsx
+++ frontend/src/component/TaskDetails/taskDetails.jsx
@@ -1,39 +1,81 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../App.css'
+import { useParams, useNavigate} from 'react-router-dom'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const taskDetails = () => {
+const TaskDetails = () => {
+    const {id} = useParams()
+    const [task, setTask] = useState('')
+    const [description, setDescription] = useState('')
+    const [dueDate, setDueDate] = useState('')
+    const [status, setStatus] = useState('')
+    const navigate = useNavigate()
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/getTask/${id}`)
+        .then(result => {
+            console.log(result.data)
+            const { task, description, dueDate, check } = result.data
+            setTask(task)
+            setDescription(description)
+            setDueDate(dueDate)
+            setStatus(check ? 'Complete' : 'Not Complete')
+        })
+        .catch(err => console.log(err))
+    }, [id])
+
+    const Update = (e) => {
+        e.preventDefault()
+        axios.put(`http://localhost:3001/updateTask/${id}`, {task, description, dueDate})
+        .then(result => {
+            console.log(result)
+            location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+
+    //Delete Items
+  const handleDelete = () => {
+    axios.delete(`http://localhost:3001/deleteTask/${id}`)
+    .then(result => {
+      navigate('/Home')
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div id='create'>
           <div className='containerCreate'>
-              <form >
-                  <h2>Task Details</h2>
-                  <div className='addD'>
-                      <label htmlFor='name'>Title</label>
-                      <input type='text' placeholder='Enter name' className='formControl' />
-                  </div>
-                  <div className='addD'>
-                      <label htmlFor='email'>Description</label>
-                      <input type='text' placeholder='Enter name' className='formControl' />
-                  </div>
-                  <div className='addD'>
-                      <label htmlFor='age'>Due Date</label>
-                      <input type='text' placeholder='Enter name' className='formControl' />
-                  </div>
-                  <div className='addD'>
-                      <label htmlFor='status'>Status</label>
-                      <p>Add the status if that home page check box task as </p>
-                  </div>
-
-                  <Link to='/Update'><button className='submitBtn'>Edit</button></Link>
-                  <button className='submitBtn'>Thwama mukuth ne</button>
-                  <button className='submitBtn'>Delete</button>
-
+              <form onSubmit={Update}>
+                <div>
+                    <h2>Task Details</h2>
+                    <Link to={'/Home'}><button>Back</button></Link>
+                </div>
+                <div className='addD'>
+                    <label htmlFor='name'>Title</label>
+                    <input type='text' placeholder='Enter Task' className='formControl' value={task || ''} onChange={(e) => setTask(e.target.value)}/>
+                </div>
+                <div className='addD'>
+                    <label htmlFor='email'>Description</label>
+                    <textarea type='text' placeholder='Enter description' className='formControl' value={description  || ''} onChange={(e) => setDescription(e.target.value)} />
+                </div>
+                <div className='addD'>
+                    <label htmlFor='age'>Due Date</label>
+                    <input type='text' placeholder='YYYY-MM-DD' className='formControl' value={dueDate  || ''} onChange={(e) => setDueDate(e.target.value)} />
+                </div>
+                <div className='addD'>
+                    <label htmlFor='status'>Status</label>
+                    <p style={{color: status === 'Complete' ? 'green' : 'red'}}>{status}</p>
+                </div>
+                <button className='submitBtn'>Update</button>
+                <button className='submitBtn' onClick={() => handleDelete(todos._id)}>Delete</button>
               </form>
           </div>
         </div>
   )
 }
 
-export default taskDetails
+export default TaskDetails
